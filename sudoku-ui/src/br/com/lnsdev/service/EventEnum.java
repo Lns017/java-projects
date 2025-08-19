ba7fea7b2ae15a35b871fe278a7fc3a6a0f405cc
Lns017 <luanjunio017@gmail.com>
2025-08-19 09:39:14 -0300

    sudoko

---
+++ sudoku-ui/src/br/com/lnsdev/service/EventEnum.java
@@ -0,0 +1,7 @@
+package br.com.lnsdev.service;
+
+public enum EventEnum {
+
+    CLEAR_SPACE
+
+}
---
+++ sudoku-ui/src/br/com/lnsdev/service/EventListener.java
@@ -0,0 +1,7 @@
+package br.com.lnsdev.service;
+
+public interface EventListener {
+
+    void update(final EventEnum eventType);
+
+}
